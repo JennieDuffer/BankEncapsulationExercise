056e173a7bf054851f1c66dee30fdcc96cb09f35
Jennie Duffer <jenniej297@gmail.com>
2022-08-19 11:02:55 -0500

    initial commit

---
+++ BankEncapsulationExercise/BankAccount.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulationExercise
+{
+    public class BankAccount
+
+    {
+        private double balance = 0;
+
+        public void Deposit(double amount)
+        {
+            balance += amount;
+        }
+
+        public double GetBalance()
+        {
+            return balance;
+        }
+
+        public void WithDraw(double amount)
+        {
+            balance -= amount;
+        }
+
+      
+
+    
+        
+        
+
+    }
+}
---
+++ BankEncapsulationExercise/Program.cs
@@ -0,0 +1,11 @@
+﻿using BankEncapsulationExercise;
+
+BankAccount myBankAccount = new BankAccount();
+
+myBankAccount.Deposit(500.00);
+Console.WriteLine(myBankAccount.GetBalance());
+
+myBankAccount.WithDraw(200.00);
+Console.WriteLine(myBankAccount.GetBalance());
+
+
